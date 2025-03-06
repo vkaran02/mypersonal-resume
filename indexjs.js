653b8fe1279505bf5e5a314f4a795fd052cb8d6c
vkaran02 <vamsi.design27@gmail.com>
2025-03-05 19:16:01 -0500

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,30 +1,6 @@
 $(document).ready(function(){
+    // Dark mode toggle functionality
     $("#themeToggleBtn").click(function(){
         $("body").toggleClass("dark-mode");
     });
 });
-
-// style for dark mode
-$('head').append(`
-    <style>
-    .dark-mode {
-        background-color: #1a202c;
-        color: #f7fafc;
-    }
-    .dark-mode .resume {
-        background-color: #2d3748;
-    }
-    .dark-mode h1, .dark-mode h2 {
-        color: #90cdf4;
-    }
-    .dark-mode .icons a {
-        color: #90cdf4;
-    }
-    .dark-mode #toggleTheme {
-        background-color: #90cdf4;
-        color: #1a202c;
-    }
-    </style>
-`);
-
-
